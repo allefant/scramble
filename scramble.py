da1c93a235fc564e3fd299825c16b8f2be1f75d2
elias <elias@d777ba90-d61a-0410-893b-cf36586ab921>
2007-03-26 15:49:48 +0000

    again
    git-svn-id: https://scramble.svn.sourceforge.net/svnroot/scramble@26 d777ba90-d61a-0410-893b-cf36586ab921

--- scramble.py
+++ scramble.py
@@ -106,7 +106,8 @@ class Translator:
                 if not tokens[0] in ["#if", "#ifdef", "#ifndef", "#endif", "#else",
                     "#undef", "#define", "#include", "#header", "#implementation",
                     "#both", "macro"]:
-                    if not tokens[1] in ["macro"]:
+                    if len(tokens) <= 1 or not (tokens[0] in ["static",
+                        "global"] and tokens[1] in ["macro"]):
                         l = l[:c]
 
             b = parenthesis_balance(l, "{", "}")
