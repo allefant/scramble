9d0ec57b5004640c53f9dd4e5a485c345a820db2
Elias Pschernig <elias@users.sourceforge.net>
2016-12-30 21:53:04 -0500

    start with module support

---
+++ eout.py
@@ -0,0 +1,19 @@
+class EWriter:
+    def write_type_info(self):
+        a = self.p.analyzer
+        for t in a.types:
+            self.code += str(t) + "\n"
+            for v in a.types[t].block.variables:
+                self.code += "    " + str(v.name) + " : " + v.get_type() + "\n"
+
+    def write_variables(self):
+        for v in self.p.root.variables:
+            if v.declaration.is_global:
+                self.code += str(v.name) + " : " + v.get_type() + "\n"
+
+    def generate(self, p):
+        self.p = p
+        self.code = ""
+        self.write_type_info()
+        self.write_variables()
+        return self.code
